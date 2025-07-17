f61ec4b95ed9de19e1677ef6d95ac8b07ecf8a21
jillur984 <jillur.cse.bd@gmail.com>
2025-07-17 10:41:50 +0600

    Implement soket IO

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,15 @@
-import express from "express"
-import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMessages, getUsersForSidebar, sendMessage } from "../controller/message.controller.js"
+import express from "express";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controller/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router=express.Router()
+const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar)
-router.get("/:id",protectRoute,getMessages)
-router.post("/send/:id",sendMessage)
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
-
-export default router
+export default router;
